161db54cece27fabd95ba928551cf9b0f3b0155e
nidhishas <nidhishas@PPMUMCPU0121.packtpub.net>
2017-10-09 15:26:26 +0530

    Commit

--- Chapter09/9-2-floats-to-flex/js/scripts.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-
-	$(".mobile-menu-icon").on("click", function(){
-		$(".primary-nav").toggleClass("active");
-		$(this).toggleClass("open");
-	});
-});//end doc ready
